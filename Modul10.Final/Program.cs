ed40acac92b33831b01de6f8bee1dba4e09a5ebd
Shatalov.AG <mrstep93@ya.ru>
2024-02-07 22:25:54 +0300

    Добавьте файлы проекта.

---
+++ Modul10.Final/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul10.Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Modul10.Task3/Messenger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul10.Task3
+{
+
+    public interface IMessenger <out T>
+    {
+        T DeviceInfo();
+    }
+
+    public class Computer { }
+    public class Phone { }
+    public class IPhone : Phone { }
+
+    public class Viber <T> :IMessenger <T> where T : Phone, new()
+    {
+
+        public T DeviceInfo()
+        {
+            T device = new T();
+            Console.WriteLine(device);
+            return new T();
+        }
+
+    }
+}
