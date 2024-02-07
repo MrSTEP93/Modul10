4a4095ec5d587542a1b214a5b01de4c259ecaca5
Shatalov.AG <mrstep93@ya.ru>
2024-02-07 22:25:54 +0300

    Добавьте файлы проекта.

---
+++ Modul10.Task3/FileManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul10.Task3
+{
+
+    public interface IWriter
+    {
+        void Write()
+        {
+            Console.WriteLine("Now we write to FILE!");
+        }
+    }
+
+    public interface IReader
+    {
+        void Read()
+        {
+            Console.WriteLine("All we read is FILE!");
+        }
+    }
+
+    public interface IMailer
+    {
+        void SendEmail()
+        {
+            Console.WriteLine("You can mail us");
+        }
+    }
+
+    public class FileManager : IWriter, IReader, IMailer
+    {
+
+    }
+}
---
+++ Modul10.Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Modul10.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //RunFileMan();
+
+            IMessenger<Phone> viberInPhone = new Viber<Phone>();
+            IMessenger<IPhone> viberInIPhone = new Viber<IPhone>();
+
+            viberInPhone.DeviceInfo();
+            viberInIPhone.DeviceInfo();
+
+            Console.Read();
+
+        }
+
+        private static void RunFileMan()
+        {
+            Console.WriteLine("Hello World!");
+            var fileMan = new FileManager();
+            ((IReader)fileMan).Read();
+        }
+    }
+}
